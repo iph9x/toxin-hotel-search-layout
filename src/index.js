77064d3bb3abceef4a0ec1864016af56c8d75a4b
iph9x <iphnx9@gmail.com>
2020-12-28 23:38:17 +0700

    colors and type

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import * as $ from 'jquery';
+import './styles/main.scss'
+
+// $('pre').addClass('green').html('asdfasdf');
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const PATHS = {
+  src: path.resolve(__dirname, 'src'),
+  dist: path.resolve(__dirname, 'dist')
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter( fileName => fileName.endsWith('.pug') );
+
+console.log("is dev: ", isDev);
+
+
+module.exports = {
+  context: PATHS.src,
+  mode: 'development',
+  entry: {
+    main: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: filename('js')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': PATHS.src
+    }
+  },  
+  devServer: {
+    contentBase: PATHS.src,
+    watchContentBase: isDev,
+    open: isDev,
+    inline: isDev,
+    hot: isDev,
+    hotOnly: isDev,
+    port: 8080,
+  },    
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          {
+            loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            options: isProd ? { publicPath: '../dist'} : {}
+          },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff|svg)$/,
+        use: ['file-loader'],
+      }
+    ]
+  },
+  plugins: [
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/, '.html')}`
+    })),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+}
