2141f83f90b09de8e9899742037f8d6d607e587c
iph9x <iphnx9@gmail.com>
2021-06-15 12:26:13 +0700

    Replace all imports to auto-imports #1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import $ from 'jquery';
-import './assets/favicons/favicon';
-import './assets/js/jquery.inputmask.min.js';
-import './assets/js/slick.min.js';
-import './assets/js/datepicker.js';
-import './assets/js/chart.min.js';
+
 import './styles/main.scss';
-import './pug/components/dropdown/dropdown.js';
-import './pug/components/rate-button/rate-button.js';
-import './pug/components/range-slider/range-slider.js';
-import './pug/components/expandable-checkbox-list/expandable-checkbox-list.js';
-import './pug/components/room-reservation/room-reservation.js';
-import './pug/components/header/header.js';
-import './pug/components/like-button/like-button.js';
-import './pug/components/room-rating-chart/room-rating-chart.js';
-import './pug/components/room-card/room-slider.js';
-import './pug/components/filter-date-dropdown/filter-date-dropdown.js';
+
+const cache = {};
+
+function importAll(r) {
+  r.keys().forEach((key) => (cache[key] = r(key)));
+}
+
+importAll(require.context('./', true, /\.js$/));
 
 const maskDate = $('.js-date-dropdown__input');
-for (let i = 0; i < maskDate.length; i++) {
+
+for (let i = 0; i < maskDate.length; i += 1) {
   Inputmask({alias: "datetime", inputFormat: "dd.mm.yyyy", "placeholder": "ДД.ММ.ГГГГ"}).mask(maskDate[i]);
 }
 
